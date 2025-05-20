40b4dd03a2e335bb7e76597f55a7c60e87b3da08
Elunded <54839409+Elunded@users.noreply.github.com>
2025-05-20 23:58:37 +0300

    Update server.js; підключив нову бд

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ require('dotenv').config();
 
 
 const express = require('express');
-const pool = require('./db'); // Підключаємо `db.js`
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // важливо!
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+}); // Підключаємо `db.js`
 const path = require('path');
 const app = express();
 app.use(express.urlencoded({ extended: false }));
