2766712e5a8bb4a7d7b14824dc65db0069c359dd
Elunded <54839409+Elunded@users.noreply.github.com>
2025-05-21 00:01:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 
 const express = require('express');
+const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
